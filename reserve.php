7fc4d12e441c12c3de992017ca04c38f770f1db1
LucaGatt22 <119947280+LucaGatt22@users.noreply.github.com>
2023-04-27 16:05:26 +0200

    - continued on validating post values - fixed some errors
    PHP SQL error:
    '''
    Fatal error: Uncaught mysqli_sql_exception: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'f,question,f)' at line 1 in D:\installations\xampp\htdocs\files\webYr1\isaacpart2\Restaurant isaac discord 13-4-23\Restaurant\database.php:48 Stack trace: #0 D:\installations\xampp\htdocs\files\webYr1\isaacpart2\Restaurant isaac discord 13-4-23\Restaurant\database.php(48): mysqli->query('INSERT INTO `fo...') #1 D:\installations\xampp\htdocs\files\webYr1\isaacpart2\Restaurant isaac discord 13-4-23\Restaurant\reserve.php(125): Db->query('INSERT INTO `fo...') #2 D:\installations\xampp\htdocs\files\webYr1\isaacpart2\Restaurant isaac discord 13-4-23\Restaurant\reserve.php(158): execSQL(Array) #3 {main} thrown in D:\installations\xampp\htdocs\files\webYr1\isaacpart2\Restaurant isaac discord 13-4-23\Restaurant\database.php on line 48
    '''

--- reserve.php
+++ reserve.php
@@ -2,10 +2,95 @@
     //This brings in a twig instance for use by this script
     require_once __DIR__.'/bootstrap.php';
     require_once __DIR__.'/database.php';
+    require_once __DIR__.'/gen-php/clean_input.php';
     
     $db = new Db();
     
+
+    function validate($formvalues){
+        if (!empty($formvalues['name']){
+            ;
+        }
+        else
+        {
+            $nameErr = "Name is required";
+            $validations['nameError'] = $nameErr;
+        }
+            
+        if (!empty($formvalues['surname']){
+            ;
+        }
+        else
+        {
+            $surnameErr = "Surname is required";
+            $validations['surnameError'] = $surnameErr;
+        }
+         
+        //Check email field
+        if (!empty($_POST["email"])) {
+            $email = clean_input($_POST["email"]);
+            //FILTER_VALIDATE_EMAIL is one of many validation filters: https://www.php.net/manual/en/filter.filters.validate.php
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            {
+                $emailErr= 'You did not enter a valid email.';
+                $validations['emailError'] = $emailErr;
+            }
+        }
+        else
+        {
+            $emailErr = "Email is required";
+            $validations['emailError'] = $emailErr;
+        }
+
+        
+        if ($_POST[]){
+            //Check message field   
+            if (!empty($_POST["message"])) {
+                $message = clean_input($_POST["message"]);
+                if(strlen($message)>150){
+                    $messageErr=  "Messages cannot be longer than 150 characters.";
+                    $validations['messageError'] = $messageErr;
+                }
+            }
+            else {
+                $messageErr = "Message is required";
+                $validations['messageError'] = $messageErr;
+            }
+        }
+
+        return $formvalues;
+    }
     
+    function execSQL($formvalues){
+        // database has 2 tables: `forms-reservation` and `forms-contact`
+        
+        // common values
+        $genVar = $formvalues['name']           .",".
+                   $formvalues['surname']     .",".
+                   $formvalues['email']       .",".
+                   $formvalues['mobile']      .","      ;
+
+        // if reservation
+        if ($_POST['dropdown-form'] === 'reservation'){
+            $result = $db -> query("INSERT INTO forms-reservation(gen-form-id,datetime) VALUES (" .
+                                   $genVar .
+                               $formvalues['datetime']
+                               .")");
+        }
+        // if message
+        else{
+            $result = $db -> query("INSERT INTO forms-contact(messageType,message) VALUES (" .
+                                   $genVar .
+                               $formvalues['messageType']    .",".
+                               $formvalues['message']
+                               .")");
+        }
+    }
+    
+
+
+
+
     if(isset($_POST['submit'])) 
     {
         
@@ -16,38 +101,17 @@
             echo "This time is already booked."
         }*/
         
+        $formvalues['name']           = clean_input( $_POST('name')       );
+        $formvalues['surname']        = clean_input( $_POST('surname')    );
+        $formvalues['email']          = clean_input( $_POST('email')      );
+        $formvalues['mobile']         = clean_input( $_POST('mobile')     );
+        $formvalues['datetime']       = clean_input( $_POST('datetime')   );
+        $formvalues['messageType']    = clean_input( $_POST('messageType'));
+        $formvalues['message']        = clean_input( $_POST('message')    );
         
-        $db -> query("INSERT INTO forms(name,surname,email,mobile-num) VALUES (" .
-                       $_POST('name')           .",".
-                       $_POST('surname')        .",".
-                       $_POST('email')          .",".
-                       $_POST('mobile')         .")");
-        
-        if ($_POST['dropdown-form'] === 'reservation'){
-            $result = $db -> query("INSERT INTO forms-reservation(gen-form-id,datetime) VALUES (" .
-                               $_POST('datetime')
-                               .")");
-        }else{
-            $result = $db -> query("INSERT INTO forms-contact(messageType,message) VALUES (" .
-                               $_POST('messageType')    .",".
-                               $_POST('message')
-                               .")");
-        }
-        
+        $formvalues = validate($formvalues);
+        execSQL($formvalues);
     }
-
-
-    /*if(isset($_POST['submit-contact']))
-    {
-        $result = $db -> query("INSERT INTO forms-contact(name,surname,type,message) VALUES (".
-                               $_POST('name-c')           .",".
-                               $_POST('surname-c')        .",".
-                               $_POST('email-address-c')  .",".
-                               $_POST('mobile-num-c')     .",".
-                               $_POST('type')             .",".
-                               $_POST('message')
-                               .")");
-    }*/
         
     
     // adds to the title tag
