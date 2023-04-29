56ba048c7bb3a0475e575c15c912e393395ce5e5
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2023-04-29 17:04:56 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,48 @@
-<?php
+    <?php
+    $error = null;
+    //This brings in a twig instance for use by this script
+    require_once __DIR__.'/bootstrap.php';
+    require_once __DIR__.'/database.php';
 
-//This brings in a twig instance for use by this script
-require_once __DIR__.'/bootstrap.php';
-require_once __DIR__.'/database.php';
+    //Load from the DB
+    $db = new Db();
 
-//Check if we need to filter
-if(isset($_GET['type'])) 
-{
-    $typeSelected = $db -> quote($_GET["type"]);
-}
-else
-{
-    $typeSelected = -1;
-}
-//Load from the DB
-$db = new Db();
+    //Check if the login form has been submitted
+    if (isset($_POST['login'])) {
+        //Get the email and password from the form
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-// adds to the title tag
-$title = "Login";
-    
-// completes the CSS filename
-$filename = "login";
+        //Check if the email exists in the database
+        $stmt = $db->prepare('SELECT * FROM Users WHERE email = ?');
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
 
-// Render view
-echo $twig->render($filename . '.html', ['title' => $title, 'filename' => $filename]);
+        if ($user) {
+            //Verify the password
+            if (password_verify($password, $user['password'])) {
+                //Successful login
+                session_start();
+                $_SESSION['user_id'] = $user['id'];
+                header('Location: /dashboard.php');
+                exit;
+            } else {
+                //Incorrect password
+                $error = 'Incorrect password';
+            }
+        } else {
+            //User not found
+            $error = 'Email not found';
+        }
+    }
 
+    // adds to the title tag
+    $title = "Login";
+        
+    // completes the CSS filename
+    $filename = "login";
+
+    // Render view
+    echo $twig->render($filename . '.html', ['title' => $title, 'filename' => $filename, 'error' => $error ?? null]);
