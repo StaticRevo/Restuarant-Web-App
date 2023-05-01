f32e4fd72b4831dd69895eaf148e1991936d7527
LucaGatt22 <luca.gatt.22@um.edu.mt>
2023-05-01 02:59:39 +0200

    SQL for reservation page form tables
    MariaDB MySQL

--- tables for forms of reservation page - project.sql
+++ tables for forms of reservation page - project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2023 at 12:59 AM
+-- Generation Time: May 01, 2023 at 02:51 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,48 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forms-contact`
+-- Table structure for table `forms_contact`
 --
 
-CREATE TABLE `forms-contact` (
-  `message-type` enum('Question','Complaint') NOT NULL,
+CREATE TABLE `forms_contact` (
+  `message_type` enum('question','complaint') NOT NULL,
   `message` text NOT NULL,
   `id` smallint(5) NOT NULL,
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `mobile-num` int(8) NOT NULL
+  `mobile_num` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Other forms to which simple message is sent';
+
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forms-reservation`
+-- Table structure for table `forms_reservation`
 --
 
-CREATE TABLE `forms-reservation` (
-  `reservation-date` text NOT NULL,
+CREATE TABLE `forms_reservation` (
+  `reservation_date` datetime NOT NULL,
   `id` smallint(5) NOT NULL,
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `mobile-num` int(8) NOT NULL
+  `mobile_num` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `forms-contact`
+-- Indexes for table `forms_contact`
 --
-ALTER TABLE `forms-contact`
+ALTER TABLE `forms_contact`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `forms-reservation`
+-- Indexes for table `forms_reservation`
 --
-ALTER TABLE `forms-reservation`
+ALTER TABLE `forms_reservation`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -73,15 +75,15 @@ ALTER TABLE `forms-reservation`
 --
 
 --
--- AUTO_INCREMENT for table `forms-contact`
+-- AUTO_INCREMENT for table `forms_contact`
 --
-ALTER TABLE `forms-contact`
+ALTER TABLE `forms_contact`
   MODIFY `id` smallint(5) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `forms-reservation`
+-- AUTO_INCREMENT for table `forms_reservation`
 --
-ALTER TABLE `forms-reservation`
+ALTER TABLE `forms_reservation`
   MODIFY `id` smallint(5) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
