1949c3cf6d44c1758b2112bebe05224a7c615dbd
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2023-05-07 18:52:14 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,48 +1,78 @@
-    <?php
-    $error = null;
-    //This brings in a twig instance for use by this script
-    require_once __DIR__.'/bootstrap.php';
-    require_once __DIR__.'/database.php';
+<?php
+session_start();
 
-    //Load from the DB
-    $db = new Db();
+$error = null;
+//This brings in a twig instance for use by this script
+require_once __DIR__.'/bootstrap.php';
+require_once __DIR__.'/database.php';
 
-    //Check if the login form has been submitted
-    if (isset($_POST['login'])) {
-        //Get the email and password from the form
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+// Load from the DB
+$db = new Db();
 
-        //Check if the email exists in the database
-        $stmt = $db->prepare('SELECT * FROM Users WHERE email = ?');
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $user = $result->fetch_assoc();
+// Check if the login form has been submitted
+if (isset($_POST['email'], $_POST['password'], $_POST['login'])) {
+    // Get the email and password from the form
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        if ($user) {
-            //Verify the password
-            if (password_verify($password, $user['password'])) {
-                //Successful login
-                session_start();
-                $_SESSION['user_id'] = $user['id'];
-                header('Location: /dashboard.php');
-                exit;
-            } else {
-                //Incorrect password
-                $error = 'Incorrect password';
-            }
+    // Check if the email exists in the database
+    $stmt = $db->prepare('SELECT * FROM Users WHERE email = ?');
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if ($user) {
+        var_dump($user); // Check the contents of the $user array
+        if (password_verify($password, $user['Password'])) {
+            // Successful login
+            $_SESSION['user_id'] = $user['id'];
+            header('Location: /Restaurant/index.php');
+            exit;
         } else {
-            //User not found
-            $error = 'Email not found';
+            // Incorrect password
+            $error = 'Incorrect password';
+            echo "Incorrect password for user with email: " . $email;
         }
+    } else {
+        // User not found
+        $error = 'Email not found';
+        echo "User not found with email: " . $email;
     }
+}
 
-    // adds to the title tag
-    $title = "Login";
-        
-    // completes the CSS filename
-    $filename = "login";
+// Set the header links based on whether the user is logged in or not
+if (isset($_SESSION['user_id'])) {
+    $headerLinks = [
+        ['url' => 'index.php', 'label' => 'Home'],
+        ['url' => 'menu.php', 'label' => 'Menu'],
+        ['url' => 'about.php', 'label' => 'About'],
+        ['url' => 'reserve.php', 'label' => 'Reservation'],
+        ['url' => 'profile.php', 'label' => 'Profile'],
+    ];
+} else {
+    $headerLinks = [
+        ['url' => 'index.php', 'label' => 'Home'],
+        ['url' => 'menu.php', 'label' => 'Menu'],
+        ['url' => 'about.php', 'label' => 'About'],
+        ['url' => 'reserve.php', 'label' => 'Reservation'],
+        ['url' => 'login.php', 'label' => 'Sign in'],
+        ['url' => 'register.php', 'label' => 'Register'],
+    ];
+}
 
-    // Render view
-    echo $twig->render($filename . '.html', ['title' => $title, 'filename' => $filename, 'error' => $error ?? null]);
+// adds to the title tag
+$title = "Login";
+
+// completes the CSS filename
+$filename = "login";
+
+// Render view
+echo $twig->render($filename . '.html', [
+    'title' => $title,
+    'filename' => $filename,
+    'error' => $error ?? null,
+    'headerLinks' => $headerLinks,
+]);
+
+
