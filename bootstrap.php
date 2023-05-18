03bb464d6e678109363bd1d3ea113d8516ae53b8
LucaGatt22 <luca.gatt.22@um.edu.mt>
2023-05-19 01:16:12 +0200

    mailer functionality in Favourites - config with SMTP - updated `database.php` to abide by structure of `config.ini`
    connection to database still works
    
    still to arrange message display.

--- bootstrap.php
+++ bootstrap.php
@@ -8,3 +8,30 @@ $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
 
 // Instantiate our Twig. 
 $twig = new \Twig\Environment($loader);
+
+
+
+
+// mail
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// SMTP settings provided by https://mailtrap.io/inboxes/2222697/messages
+$mail = new PHPMailer();
+$mail->isSMTP();
+
+$mailConfig = parse_ini_file('./config.ini',true)['mailer'];
+$mail->Host     = $mailConfig['host'];
+$mail->SMTPAuth = $mailConfig['SMTPAuth'];
+$mail->Port     = $mailConfig['port'];
+$mail->Username = $mailConfig['username'];
+$mail->Password = $mailConfig['password'];
+
+// Content
+$mail->setFrom($mailConfig['fromAddress']);
+$mail->isHTML($mailConfig['isHTML']);                                  //Set email format to HTML
+    
--- database.php
+++ database.php
@@ -22,7 +22,7 @@ class Db {
         if(!isset(self::$connection)) {
             // Load configuration as an array. Use the actual location of your configuration file
             // Put the configuration file outside of the document root
-            $config = parse_ini_file('./config.ini'); 
+            $config = parse_ini_file('./config.ini',true)['database'];
             self::$connection = new mysqli($config['server'],$config['username'],$config['password'],$config['dbname']);
         }
     
