b279dbdd1c2f500a96a2adf7aa55fec54fb37cfa
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2023-05-26 09:00:44 +0200

    Update favourites.php

--- favourites.php
+++ favourites.php
@@ -9,8 +9,34 @@ require_once 'gen-php/clean_input.php';
 require_once 'gen-php/validate.php';
 
 
+// Get the user ID from the session
+$user_id = $_SESSION['user_id'];
 
-/*updated*/
+// Get the menu item ID from the query parameter
+$menu_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+if ($menu_id !== false) {
+    // Get db object
+    $db = new Db();
+
+    // Check if the menu item ID is not null
+    if ($menu_id !== null) {
+        // Prepare and execute the SQL statement to insert into the "favourites" table
+        if ($user_id !== null) {
+            $stmt = $db->prepare('INSERT INTO favourites (userID, menuitemID) VALUES (?, ?)');
+            $stmt->bind_param('ii', $user_id, $menu_id);
+            $stmt->execute();
+        }
+    }
+    // Redirect back to the menu details page
+    header("Location: menudetails.php?id=$menu_id");
+    exit();
+} else {
+    // Invalid menu item ID
+    echo 'Invalid menu item ID.';
+}
+
+/*updated Luca Code*/
 if(isset($_POST['submit-favourites'])){
     $formvalues['email'] = clean_input($_POST['email']);
     [$formvalues, $validations] = validateEmail($formvalues, $validations);
